86be471018bb5aebd645cf3383dfc08c1e0e399b
Kareem <kareemalgalaly@berkeley.edu>
2024-12-30 18:01:54 -0500

    improved infra

--- attack/cnn/regress.py
+++ attack/cnn/regress.py
@@ -3,7 +3,9 @@ from cnn_gen import GenericCNN
 import json
 
 import os
+import sys
 import time
+import hashlib
 import datetime
 
 import torch
@@ -11,31 +13,117 @@ import torch.nn as nn
 import torch.optim as optim
 import matplotlib.pyplot as plt
 
+from copy import copy
+
+import argparse
+
+argparser = argparse.ArgumentParser(prog="regress.py", description="Run cnn regressions")
+argparser.add_argument("-i", "--json",   type=str, default='regression.json', help="JSON description of CNNs")
+argparser.add_argument("-o", "--output", type=str, default='outputs', help="Directory for output files")
+argparser.add_argument("-n", "--nowrite", const=True, default=False, action='store_const', help="Don't write any outputs")
+argparser.add_argument("-f", "--force", const=True, default=False, action='store_const', help="Overwrite output files")
+argparser.add_argument("-p", "--preview", const=True, default=False, action='store_const', help="Don't run anything only list runs that would occur")
+
 pwd      = os.path.dirname(os.path.abspath(__file__))
 proj_dir = os.path.dirname(os.path.dirname(pwd))
 data_dir = os.path.join(proj_dir, 'analog', 'outfiles')
 
-# Base ###########################################
+# Helpers ########################################
+
+class ProgressBar:
+    def __init__(self, f_start="", f_end="", bar_len=30, bar_chr='X', max_val=1, out=sys.stdout):
+        self.out     = out
+        self.f_start = f_start
+        self.f_end   = f_end
+        self.bar_len = bar_len
+        self.bar_chr = bar_chr
+        self.max_val = max_val
+
+        self.val_len = len(str(max_val))
+        #self.start_args = None
+        #self.stop_args = None
+        self.kwargs = {}
+
+        self.running = False
+
+    def start(self, **kwargs):
+        self.kwargs = kwargs
+        self.running = True
+        self.update(0)
+
+    def update(self, value, **kwargs):
+        if not(self.running): return
+
+        done = int(self.bar_len * value / self.max_val)
+        remn = self.bar_len - done
+
+        if kwargs is not {}: self.kwargs.update(kwargs)
+
+        print(f"{self.f_start.format(**self.kwargs)}[{self.bar_chr*done}{('-'*(remn))}] {value:{self.val_len}}/{self.max_val} {self.f_end.format(**self.kwargs)}", end='\r', file=self.out, flush=True)
+
+    def stop(self):
+        if not(self.running): return
+
+        print(f"{self.f_start.format(**self.kwargs)}[{self.bar_chr*self.bar_len}] {self.max_val}/{self.max_val} {self.f_end.format(**self.kwargs)}", end='\n', file=self.out, flush=True)
+
+        self.kwargs = {}
+        self.running = False
+
+# bar = ProgressBar(f_start="Training {}", max_val=100)
+# bar.start(("NN"))
+# plt.pause(1)
+# bar.update(10)
+# plt.pause(1)
+# bar.update(30)
+# plt.pause(1)
+# bar.stop()
+
+# Base36 Hash ####################################
 
 base36char = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
 def base36(number):
     result = ""
     number = abs(number)
-
     while number != 0:
         number, remainder = divmod(number, 36)
         result += base36char[remainder]
-
     return result or base36char[0]
+
+def base36hash(string):
+    o = hashlib.shake_128(string.encode('ascii'))
+    b = o.digest(9)
+    h = ""
+
+    for i in range(0, len(b), 9):
+        v = 0
+        m = 1
+
+        for j in range(0, 9):
+            if i+j < len(b):
+                #print("byte", j+i, b[i+j], m)
+                v += b[j] * m
+            else:
+                break
+            m = m << 8
+
+        h += base36(v).rjust(14,"0")
+        #print(h)
+        #print(o.hexdigest(16))
+    return h
+
+    # 36 ^ 7 > 16 ^ 9
+    # 36 ^ 14 > 16 ^ 18
+
+# Base ###########################################
 
 class HashableBase:
     def __str__(self):
         csv = self.get_csv()
-        return f"{base36(hash(csv))},{csv}"
+        return f"{base36hash(csv)},{csv}"
 
-    def __hash__(self):
-        return hash(self.get_csv())
+    def hash(self):
+        return base36hash(self.get_csv())
 
     def get_csv(self):
         raise NotImplementedError("HashableBase should not be instantiated directly")
@@ -53,7 +141,13 @@ class Network(HashableBase):
         return f"{self.type},{self.definition},{self.inputs}"
 
     def create(self, input_len, input_ch):
-        return GenericCNN(f"{input_len},{input_ch}:{self.definition}")
+        desc = f"{input_len},{input_ch}:{self.definition}"
+        try:
+            return GenericCNN(desc)
+        except Exception as e:
+            print("Failed to create:", desc)
+            print(e)
+            return None
 
 # Dataset ########################################
 
@@ -104,7 +198,7 @@ class SampledDataset(Dataset):
         self.mode     =  info['sample_mode']
         self.interval =  info['sample_interval']
         self.duration =  info['sample_duration']
-        self.len      = self.duration / self.interval
+        self.len      = int(self.duration / self.interval)
 
     def get_csv(self):
         return f"{super().get_csv()},{self.mode};{self.interval};{self.duration}"
@@ -132,8 +226,21 @@ class Test(HashableBase):
         #if not isinstance(self.learning_rate, list): self.learning_rate = [self.learning_rate]
         #if not isinstance(self.optimizer,     list): self.optimizer     = [self.optimizer]
 
+    def __copy__(self):
+        cls  = type(self)
+        that = cls.__new__(cls)
+        that.networks = self.networks
+        that.datasets = self.datasets
+        that.learning_rate = self.learning_rate
+        that.optimizer     = self.optimizer
+        that.max_epochs    = self.max_epochs
+        that.max_accuracy  = self.max_accuracy
+        that.max_loss      = self.max_loss
+        that.batch_size    = self.batch_size
+        return that
+
     def get_csv(self):
-        return f"{self.learning_rate},{self.optimizer},{self.batch_size}.{self.max_epochs},{self.max_accuracy},{self.max_loss}"
+        return f"{self.learning_rate},{self.optimizer},{self.batch_size},{self.max_epochs},{self.max_accuracy},{self.max_loss}"
 
     def get_optimizer(self, cnn):
         if self.optimizer == 'Adam':
@@ -165,11 +272,25 @@ class Regression:
 
         self.networks = {name: Network(name, info) for name, info in self.dict['networks'].items()}
         self.datasets = {name: Dataset.from_info(name, info) for name, info in self.dict['datasets'].items()}
-        self.tests =    [Test(info, self.networks, self.datasets, self.defaults) for info in self.dict['tests']]
+        self.build_tests(self.dict['tests'])
+
+    def build_tests(self, tests):
+        self.tests = []
+
+        for info in tests:
+            test = Test(info, self.networks, self.datasets, self.defaults)
+
+            if isinstance(test.learning_rate, list):
+                for lr in test.learning_rate:
+                    test_i = copy(test)
+                    test_i.learning_rate = lr
+                    self.tests.append(test_i)
+            else:
+                self.tests.append(test)
 
     def run_all(self):
         device = torch.device("cuda") if torch.cuda.is_available() else None
-        plt.ion()
+        if not(args.preview): plt.ion()
 
         for test in self.tests:
             for dataset in test.datasets:
@@ -178,23 +299,30 @@ class Regression:
                 for network in test.networks:
                     assert network.inputs == dataset.cols
 
-                    fig, axs = plt.subplots(2, figsize=(8,8))
-                    axs[0].set_title("Loss")
-                    axs[1].set_title("Accuracy")
+                    if not(args.preview):
+                        fig, axs = plt.subplots(2, figsize=(8,8))
+                        axs[0].set_title("Loss")
+                        axs[1].set_title("Accuracy")
+                    else:
+                        axs = None
     
+                    run_hash = base36(hash(network.get_csv() + dataset.get_csv() + test.get_csv()))
+                    print(f"{run_hash},{network.name},{dataset.name},{test}")
+
                     if network.type == 'bitwise':
                         for i in range(self.adc_bitwidth-1, -1, -1):
-                            self.run_eval_cnn(test, network, dataset, device, axs, bit=i)
+                            self.run_eval_cnn(test, network, dataset, device, f"{run_hash}_{i}", axs, bit=i)
                     else:
-                        self.run_eval_cnn(test, network, dataset, device, axs, bit=-1)
+                        self.run_eval_cnn(test, network, dataset, device, run_hash, axs, bit=-1)
 
-                    run_hash = base36(hash(str(network) + str(dataset)))
-                    fig.savefig(f'outputs/{run_hash}.png')
-                    plt.close()
+                    if not(args.preview) and not(args.nowrite):
+                        fig.savefig(f'{args.output}/{run_hash}.png')
+                        plt.close()
 
 
-    def run_eval_cnn(self, test, network, dataset, device, axs, bit=-1):
+    def run_eval_cnn(self, test, network, dataset, device, run_hash, axs, bit=-1):
         plot_period = 1000 if device else 10
+        acc_period  = 100  if device else 10
 
         start_tm = time.monotonic()
         # if ? or self.overwrite
@@ -208,19 +336,22 @@ class Regression:
 
         if bit == self.adc_bitwidth-1 or bit == -1: print(cnn)
 
-        run_hash = base36(hash(str(network) + str(dataset))) + f"_{bit}"
-        print(f"{run_hash},{network.name},{network},{dataset.name},{dataset},{test},{bit}")
+        if (cnn is None): raise RuntimeError("Failed to build CNN")
+        if (args.preview): return
 
         cnn = cnn.to(device)
     
         loss_arr = torch.empty(test.max_epochs, device=device)
-        acc_arr  = torch.empty(test.max_epochs//plot_period, device=device)
+        acc_arr  = torch.empty(test.max_epochs//acc_period, device=device)
         loss_g = None
         acc_g  = None
 
         criterion = nn.CrossEntropyLoss()
         optimizer = test.get_optimizer(cnn)
     
+        progress = ProgressBar(f_start="Training ", f_end="{model} | Loss {loss:8} | Accuracy {acc:8} | {msg}", max_val=test.max_epochs)
+        progress.start(model=run_hash, loss=1.0, acc=0.0, msg="          ")
+
         for epoch in range(test.max_epochs):
             correct = 0
 
@@ -242,18 +373,21 @@ class Regression:
 
                 # Calculate Accuracy
 
-                if (epoch % plot_period) == 0:
+                if (epoch % acc_period) == 0:
                     _, predicted = torch.max(output, 1)
                     correct += (predicted == labels).sum()
 
             loss_arr[epoch] = loss
 
-            if epoch % plot_period == 0:
+            if epoch % acc_period == 0:
                 accuracy = correct / len(dataset.builder.dataset)
-                acc_indx = epoch//plot_period
+                acc_indx = epoch//acc_period
                 acc_arr[acc_indx] = accuracy
-                print(f'TRAINING: {run_hash}, Epoch {epoch:5}, Loss:     {loss.item()}')
-                print(f'TRAINING: {run_hash}, Epoch {epoch:5}, Accuracy: {accuracy}')
+
+            if epoch % plot_period == 0:
+                #print(f'TRAINING: {run_hash}, Epoch {epoch:5}, Loss:     {loss.item()}')
+                #print(f'TRAINING: {run_hash}, Epoch {epoch:5}, Accuracy: {accuracy}')
+                progress.update(epoch, loss=round(loss.item(), 6), acc=round(float(accuracy),6))
 
                 if loss_g: loss_g.remove()
                 if acc_g:  acc_g.remove()
@@ -262,30 +396,38 @@ class Regression:
                 plt.pause(0.01)
 
             if accuracy >= test.max_accuracy:
-                print("Reached target accuracy"); break
+                progress.update(epoch, msg="Reached target accuracy"); break
 
             if loss <= test.max_loss:
-                print("Reached target loss"); break
-
-        torch.save(cnn.state_dict(), f'outputs/{run_hash}.state')
+                progress.update(epoch, msg="Reached target loss"); break
+        
+        progress.stop()
 
         label = f'cnn[{bit}]'
         axs[0].plot(loss_arr.detach().cpu()[:epoch], label=label)
-        axs[1].plot(acc_arr.cpu()[:epoch//plot_period+1], label=label)
+        axs[1].plot(acc_arr.cpu()[:epoch//acc_period+1], label=label)
         axs[0].legend()
         axs[1].legend()
         plt.pause(0.01)
 
+        if args.nowrite: return
+
         stop_tm = time.monotonic()
         runtime = stop_tm - start_tm
 
-        with open(f'outputs/run_results.csv', 'a') as file:
+        torch.save(cnn.state_dict(), f'{args.output}/{run_hash}.state')
+
+        with open(f'{args.output}/run_results.csv', 'a') as file:
             file.write(f"{run_hash},{network.name},{network},{dataset.name},{dataset},{test},{bit},{accuracy},{loss},{epoch},{runtime}\n")
 
 # Main ###########################################
 
 if __name__  == '__main__':
-    regression = Regression('regression.json')
+    args = argparser.parse_args()
+
+    os.makedirs(args.output, exist_ok=True)
+
+    regression = Regression(args.json)
     regression.load()
     regression.run_all()
 
