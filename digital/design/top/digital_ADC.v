d045b844ce4da0fe4cca978c8ccbbaf3fb5e78e7
Kareem Ahmad <kareemahmad@Kareems-Laptop.local>
2025-01-28 21:08:12 -0500

    cleanup ports

--- digital/design/top/digital_ADC.v
+++ digital/design/top/digital_ADC.v
@@ -1,15 +1,17 @@
 module digitalADC #(
     parameter ASYNC = 0,
-    parameter NUM_SENSORS = 1,
     parameter WIDTH = 8
 ) (
-    input  clk,
-    input  rst,
-    input  comp_out, // [NUM_SENSORS-1:0]
+    input               clk,
+    input               rst,
+    input               comp_out,
 
-    output reg valid, // [NUM_SENSORS-1:0]
-    output reg [WIDTH-1:0] digital_val, // [NUM_SENSORS-1:0]
+    output              valid,       
+    output [WIDTH-1:0]  digital_val
 );
+
+    wire [WIDTH-1:0] count_value;
+    wire             comp_pulsed;
 
     counter_half #(
         .WIDTH(WIDTH)
