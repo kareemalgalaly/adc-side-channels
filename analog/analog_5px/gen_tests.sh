b1a5760911b465ba6e6c4f1fe013dc96e3f40bc9
Kareem Ahmad <kahmad8@gatech.edu>
2025-02-19 15:28:47 -0500

    add randramp (q) option for analog testcases. update gen_seeds to enable generating switch values (--ticks, --prefix, --allow_duplicates)

--- analog/analog_5px/gen_tests.sh
+++ analog/analog_5px/gen_tests.sh
@@ -16,10 +16,16 @@ if [ "$1" = "1px" ]; then
     flavors=( xx px xm pm )
     seeds=(0)
     parg="pixels=eval:1"
+elif [ "$1" = "1qx" ]; then
+    pixels="1px"
+    corners=( tt ) # ss ff sf fs 
+    flavors=( qx )
+    seeds=(0 256)
+    parg="pixels=eval:1"
 elif [ "$1" = "5px" ]; then
     pixels="5px"
     corners=( tt ) # ss ff sf fs 
-    flavors=( xx px xm pm )
+    flavors=( xx px xm pm qx qm)
     seeds=(0 128 256 384)
     parg="pixels=eval:5"
 elif [ "$1" = "run" ]; then
@@ -32,7 +38,8 @@ fi
 shift 1
 
 for seed in ${seeds[@]}; do
-    [ -f seeds_$seed ] || python ../../script/gen_seeds.py -s 0 -p 5 -i 1 -r 1 -n 128 --start $seed > seeds_$seed
+    [ -f seeds_$seed ]    || python ../../script/gen_seeds.py -s 0 -p 5 -i 1 -r 1           -n 128                 --start $seed > seeds_$seed
+    [ -f seeds_rr_$seed ] || python ../../script/gen_seeds.py -s 0 -p 5 -i 0 -r 1.8 -t 2 -d -n 256 --prefix randvc --start $seed > seeds_rr_$seed
 done
 
 if [ -f /foss/pdk/sky130A/libs.tech/ngspice/sky130.lib.spice ]; then
@@ -43,17 +50,20 @@ fi
 
 
 for flavor in ${flavors[@]}; do
-    case flavor in 
-        xx ) flv="" ;;
-        px ) flv="protected=" ;;
-        xm ) flv="mismatch=" ;;
-        pm ) flv="protected= mismatch=" ;;
+    case $flavor in 
+        xx ) flv=""                               ;;
+        px ) flv="protected="                     ;;
+        xm ) flv="mismatch="                      ;;
+        pm ) flv="protected= mismatch="           ;;
+        qx ) flv="protected= randramp="           ;;
+        qm ) flv="protected= randramp= mismatch=" ;;
+        *  ) echo "nomatch <$flavor>"
     esac
 
-    args=("randvec_1=compose randvec_1 values 0.1 1.0 0.6" "randvec_2=compose randvec_2 values 0.3 0.0 0.6" "randvec_3=compose randvec_3 values 0.5 0.2 0.6" "randvec_4=compose randvec_4 values 0.7 0.8 0.6" "randvec_5=compose randvec_5 values 0.9 0.4 0.6")
+    args=("randvec_1=compose randvec_1 values 0.1 1.0 0.6" "randvec_2=compose randvec_2 values 0.3 0.0 0.6" "randvec_3=compose randvec_3 values 0.5 0.2 0.6" "randvec_4=compose randvec_4 values 0.7 0.8 0.6" "randvec_5=compose randvec_5 values 0.9 0.4 0.6" "randvc0=compose randvc0 values 0 1.8 0" "randvc1=compose randvc1 values 1.8 0 1.8" "randvc2=compose randvc2 values 0 1.8 1.8" "randvc3=compose randvc3 values 0 0 0" "randvc4=compose randvc4 values 0 1.8 0" "seed=0")
 
     echo Building demo_${pixels}_${corners[0]}_${flavor}_${seeds[0]}.cir
-    python ../../script/template_engine.py adc_sky_5px.temp.cir -s plot= "${args[@]}" $parg -o demo_${pixels}_${corners[0]}_${flavor}_${seeds[0]}.cir
+    python ../../script/template_engine.py adc_sky_5px.temp.cir -s plot= $flv "${args[@]}" $parg -o demo_${pixels}_${corners[0]}_${flavor}_${seeds[0]}.cir
     echo
 
     for corner in ${corners[@]}; do
@@ -61,20 +71,25 @@ for flavor in ${flavors[@]}; do
             args=()
             if [ "$pixels" = 1px ]; then
                 args+=("randvec_1=eval:'compose randvec_1 values '+' '.join(str(i/256) for i in range(256))")
+                while read -r i ; do 
+                    args+=("$i")
+                done < seeds_rr_$seed
             else
                 while read -r i ; do 
                     args+=("$i")
                 done < seeds_$seed
+                while read -r i ; do 
+                    args+=("$i")
+                done < seeds_rr_$seed
             fi
             args+=("corner=$corner")
             args+=("outdir=outfiles/analog_${pixels}_${corner}_${flavor}")
-            args+=("$flv")
             args+=("seed=$seed")
             args+=("$foss")
             args+=("$parg")
 
             echo Building runme_${pixels}_${corner}_${flavor}_${seed}.cir
-            python ../../script/template_engine.py adc_sky_5px.temp.cir -s "${args[@]}" -o runme_${pixels}_${corner}_${flavor}_${seed}.cir
+            python ../../script/template_engine.py adc_sky_5px.temp.cir -s "${args[@]}" $flv -o runme_${pixels}_${corner}_${flavor}_${seed}.cir
             echo
         done
     done
--- script/gen_seeds.py
+++ script/gen_seeds.py
@@ -9,15 +9,20 @@ argparser.add_argument("-s", "--seed",    type=int, default=0  , help="Seed")
 argparser.add_argument("-p", "--pixels",  type=int, default=1  , help="Number of pixels")
 argparser.add_argument("-n", "--num",     type=int, default=50 , help="Number of random seeds")
 argparser.add_argument("-r", "--range",   type=float, default=256, help="Number of random seeds")
+argparser.add_argument("-t", "--ticks",   type=int, default=256, help="Resolution of range")
 argparser.add_argument(      "--start",   type=int, default=0  , help="Start point for saving num seeds")
 argparser.add_argument("-i", "--index",   type=int, default=0  , help="Initial index for output naming")
+argparser.add_argument(      "--prefix",  type=str, default="randvec_", help="Variable name prefix")
+argparser.add_argument("-d", "--allow_duplicates", default=False, const=True, action="store_const", help="Allow duplicate combinations")
 args = argparser.parse_args()
 
 random.seed(args.seed)
 
 if args.range > 1:
     def get_rval():
-        return tuple(random.randint(0, int(args.range)) for j in range(args.pixels))
+        ticks = int(args.ticks - 1)
+        return tuple((random.randint(0, ticks)/ticks) * args.range for j in range(args.pixels))
+        #return tuple(random.randint(0, int(args.range)) for j in range(args.pixels))
 else:
     def get_rval():
         return tuple(random.random()*args.range for j in range(args.pixels))
@@ -32,11 +37,17 @@ def get_rvals():
     rvals = []
     seen = set()
 
-    for i in range(args.start):
-        get_unique_rval(seen)
+    if args.allow_duplicates:
+        for i in range(args.start):
+            get_rval()
+        for i in range(args.num):
+            rvals.append(get_rval())
 
-    for i in range(args.num):
-        rvals.append(get_unique_rval(seen))
+    else:
+        for i in range(args.start):
+            get_unique_rval(seen)
+        for i in range(args.num):
+            rvals.append(get_unique_rval(seen))
 
     return rvals
 
@@ -48,7 +59,7 @@ def generate_rval_strings():
     strings = []
     for i, vec in enumerate(vectors):
         #strings[f"pixelvec_{i}"] = f"compose pixelvec_{i} values {' '.join(str(v) for v in vec)}"
-        strings.append(f"randvec_{i+args.index}=compose randvec_{i+args.index} values {' '.join(str(v) for v in vec)}")
+        strings.append(f"{args.prefix}{i+args.index}=compose {args.prefix}{i+args.index} values {' '.join(str(v) for v in vec)}")
 
     return strings
 
