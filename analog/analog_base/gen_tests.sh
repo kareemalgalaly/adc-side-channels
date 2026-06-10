c3d7afd7bdb276c1ff7ca2dccc29659709f31973
Kareem Ahmad <kahmad8@gatech.edu>
2026-06-10 12:16:20 -0400

    get analog_base working with updated teng

--- analog/analog_base/gen_tests.sh
+++ analog/analog_base/gen_tests.sh
@@ -14,27 +14,31 @@ if [ "$1" = "1px" ]; then
     pixels="1px"
     #corners=( tt ) # ss ff sf fs 
     #flavors=( xl pl ) # xx px xm pm 
-    corners=( tt ) # ss ff sf fs 
-    flavors=( xx xm xl ) # xx px xm pm 
-    seeds=(0)
+    corners=( tt ss fs ) # ss ff sf fs 
+    flavors=( xx ) # xx px xm pm 
+    seeds=(0 64 128 192)
+    scnt=64
     parg="pixels=eval:1"
 elif [ "$1" = "1qx" ]; then
     pixels="1px"
     corners=( tt ) # ss ff sf fs 
     flavors=( qx )
     seeds=(0 256)
+    scnt=256
     parg="pixels=eval:1"
 elif [ "$1" = "1dx" ]; then
     pixels="1px"
     corners=( tt ) # ss ff sf fs 
     flavors=( dx )
     seeds=(0 256)
+    scnt=256
     parg="pixels=eval:1"
 elif [ "$1" = "5px" ]; then
     pixels="5px"
     corners=( tt ) # ss ff sf fs 
     flavors=( xx px xm pm qx qm)
     seeds=(0 128 256 384)
+    scnt=256
     parg="pixels=eval:5"
 elif [ "$1" = "run" ]; then
     for file in $(ls runme*cir); do echo ngspice $file; ngspice $file; done
@@ -46,8 +50,8 @@ fi
 shift 1
 
 for seed in ${seeds[@]}; do
-    [ -f seeds_$seed ]    || python ../../script/gen_seeds.py -s 0 -p 5 -i 1 -r 1           -n 128                 --start $seed > seeds_$seed
-    [ -f seeds_rr_$seed ] || python ../../script/gen_seeds.py -s 0 -p 5 -i 0 -r 1.8 -t 2 -d -n 512 --prefix randvc --start $seed > seeds_rr_$seed
+    [ -f seeds_$seed ]    || python ../../script/gen_seeds.py -s 0 -p 5 -i 1 -r 1           -n $scnt         --start $seed > seeds_$seed
+    [ -f seeds_rr_$seed ] || python ../../script/gen_seeds.py -s 0 -p 5 -i 0 -r 1.8 -t 2 -d -n $((scnt * 4)) --start $seed --prefix randvc > seeds_rr_$seed
 done
 
 if [ -f /foss/pdk/sky130A/libs.tech/ngspice/sky130.lib.spice ]; then
@@ -71,10 +75,10 @@ for flavor in ${flavors[@]}; do
         *  ) echo "nomatch <$flavor>"
     esac
 
-    args=("randvec_1=compose randvec_1 values 0.1 1.0 0.6" "randvec_2=compose randvec_2 values 0.3 0.0 0.6" "randvec_3=compose randvec_3 values 0.5 0.2 0.6" "randvec_4=compose randvec_4 values 0.7 0.8 0.6" "randvec_5=compose randvec_5 values 0.9 0.4 0.6" "randvc0=compose randvc0 values 0 1.8 0" "randvc1=compose randvc1 values 1.8 0 1.8" "randvc2=compose randvc2 values 0 1.8 1.8" "randvc3=compose randvc3 values 0 0 0" "randvc4=compose randvc4 values 0 1.8 0" "seed=0")
+    args=("randvec_1=compose randvec_1 values 0.1 1.0 0.6" "randvec_2=compose randvec_2 values 0.3 0.0 0.6" "randvec_3=compose randvec_3 values 0.5 0.2 0.6" "randvec_4=compose randvec_4 values 0.7 0.8 0.6" "randvec_5=compose randvec_5 values 0.9 0.4 0.6" "randvc0=compose randvc0 values 0 1.8 0" "randvc1=compose randvc1 values 1.8 0 1.8" "randvc2=compose randvc2 values 0 1.8 1.8" "randvc3=compose randvc3 values 0 0 0" "randvc4=compose randvc4 values 0 1.8 0" "seed=0" "start=0" "stop=3")
 
     echo Building demo_${pixels}_${corners[0]}_${flavor}_${seeds[0]}.cir
-    python ../../script/template_engine.py adc_sky_5px.temp.cir -s plot= $flv "${args[@]}" $parg -o demo_${pixels}_${corners[0]}_${flavor}_${seeds[0]}.cir
+    python ../../script/teng.py -o demo_${pixels}_${corners[0]}_${flavor}_${seeds[0]}.cir adc_sky_5px.temp.cir plot= $flv "${args[@]}" $parg 
     echo
 
     for corner in ${corners[@]}; do
@@ -98,9 +102,11 @@ for flavor in ${flavors[@]}; do
             args+=("seed=$seed")
             args+=("$foss")
             args+=("$parg")
+            args+=("start=$seed")
+            args+=("stop=$((scnt+seed))")
 
             echo Building runme_${pixels}_${corner}_${flavor}_${seed}.cir
-            python ../../script/template_engine.py adc_sky_5px.temp.cir -s "${args[@]}" $flv -o runme_${pixels}_${corner}_${flavor}_${seed}.cir
+            python ../../script/teng.py -o runme_${pixels}_${corner}_${flavor}_${seed}.cir adc_sky_5px.temp.cir "${args[@]}" $flv 
             echo
         done
     done
